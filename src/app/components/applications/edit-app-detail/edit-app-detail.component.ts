8c630ef9567bbec7bcb061456c43b6ccf31f6899
Alex Tkachenko <alex@softkit.company>
2020-10-23 11:11:58 +0300

    remove commented code

--- src/app/components/applications/edit-app-detail/edit-app-detail.component.ts
+++ src/app/components/applications/edit-app-detail/edit-app-detail.component.ts
@@ -76,19 +76,6 @@ export class EditAppDetailComponent implements OnInit {
     }
 
 
-    // this.productImages=this.appDetails.customData.product__image__file;
-    // this.icons=[this.appDetails.customData.icon__file];
-    // FroalaEditor.DefineIcon('alert', {NAME: 'info'});
-    // FroalaEditor.RegisterCommand('alert', {
-    //   title: 'Hello',
-    //   focus: false,
-    //   undo: false,
-    //   refreshAfterCallback: false,
-    //
-    //   callback: () => {
-    //     alert('Hello!');
-    //   }
-    // });
     this.getyouTubeId();
   }
 
