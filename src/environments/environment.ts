5896759b91860da1ed72eebdc720750dd4ae4cbe
Alex Tkachenko <alex@softkit.company>
2020-11-25 13:47:39 +0200

    remove unused properties

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8091/',
-  client_id: '43m9qbjigr8ed6i1het1rdnfso',
-  client_secret: 'on5jmqp7uq00vgeg9tnq4pii2f1f3pm84q45uq907e8vhmr24ta',
-  // client_id:'5f20f800579d1a20c527d848',
-  // client_secret:'5OqlDMSXRQX4aWCp5yWw8IT7RP-m1lhX2L0zSy4Ttj8',
-  openchannelUrl: 'https://market.openchannel.io',
-  graphqlUrl: 'http://6e4d6c7f6f0f.ngrok.io/graphql',
-  // graphqlUrl: 'http://60da39e541f9.ngrok.io/graphql',
-  auth: 'Basic NWY1NGE3NjljOTZjZjYxMjNlNzNiMDZmOmVJUG1fQ3F5NXVTVm1vbXdLNlNLakh1cEdQX3drN0hEY29kSS13bkZuUm8=',
   iframelyApiKey: 'ab8c8d0627356d139eed8f',
   iframelyUrl: 'https://iframe.ly/api/iframely',
 };
