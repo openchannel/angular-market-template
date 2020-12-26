6b375d364dcdc34e57ad5617c58b2c36a7f6a32e
Alex Tkachenko <alex@softkit.company>
2020-12-26 14:17:48 +0200

    added my apps page

--- src/app/pages/account-management/account-management-routing.module.ts
+++ src/app/pages/account-management/account-management-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MyProfileComponent} from './my-profile/my-profile.component';
+import {MyAppsComponent} from './my-apps/my-apps.component';
 
 const routes: Routes = [
   {path: 'profile', component: MyProfileComponent},
+  {path: 'apps', component: MyAppsComponent},
 ];
 
 @NgModule({
