bbff70053273da19c9dde9cde0ac7a8e7fa1477d
Vitaliy Samofal <vsamofal93@gmail.com>
2021-02-02 20:31:03 +0200

    TE-671 Refresh JWT token into AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthenticationService, TitleService} from 'oc-ng-common-service';
-import {takeUntil} from 'rxjs/operators';
+import {first, takeUntil} from 'rxjs/operators';
 import {Subject} from 'rxjs';
+import {LoadingBarState} from '@ngx-loading-bar/core/loading-bar.state';
+import {LoadingBarService} from '@ngx-loading-bar/core';
 
 @Component({
   selector: 'app-root',
@@ -17,16 +19,26 @@ import {Subject} from 'rxjs';
 export class AppComponent implements OnInit {
 
   private destroy$: Subject<void> = new Subject();
+  private loader: LoadingBarState;
 
   constructor(public router: Router,
               private authenticationService: AuthenticationService,
-              private titleService: TitleService) {
+              private titleService: TitleService,
+              public loadingBar: LoadingBarService) {
+    this.loader = this.loadingBar.useRef();
   }
 
   ngOnInit() {
     // refresh JWT token if exists
+    this.loader.start();
     this.authenticationService.isLoggedUserByAccessOrRefreshToken()
     .pipe(takeUntil(this.destroy$))
-    .subscribe(() => {});
+    .subscribe(() => this.loader.stop(), () => this.loader.stop());
+
+    // init csrf
+    this.authenticationService.initCsrf()
+    .pipe(takeUntil(this.destroy$), first())
+    .subscribe(() => {
+    });
   }
 }
