721da7b2a97cfc4da93771ea0f67c38bbd832e8b
Julia <denisenko.sk@gmail.com>
2021-02-12 13:33:30 +0200

    TE-736: Updated the Header for the new Profile navbar component style

--- src/app/pages/template/header/header.component.ts
+++ src/app/pages/template/header/header.component.ts
@@ -14,7 +14,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   isSSO: any;
   isSsoConfigExist = true;
-  menuItems: DropdownModel<string> [] = [];
 
   private destroy$: Subject<void> = new Subject();
 
@@ -32,7 +31,6 @@ export class HeaderComponent implements OnInit, OnDestroy {
         takeUntil(this.destroy$),
         map(value => !!value))
       .subscribe((authConfigExistence) => this.isSsoConfigExist = authConfigExistence);
-    this.generateDropdownMenuItems();
   }
 
   ngOnDestroy(): void {
@@ -44,26 +42,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.router.navigate(['/login'], { queryParams: { returnUrl: this.router.routerState.snapshot.url }});
   }
 
-  generateDropdownMenuItems() {
-    if (!this.isSSO) {
-      this.menuItems.push({
-        value: '/management/profile',
-        label: 'My Profile'
-      });
-    }
-    if (this.authHolderService.userDetails.role === 'ADMIN') {
-      this.menuItems.push({
-        value: '/management/company',
-        label: 'My Company'
-      });
-    }
-  }
-
-  onLogout() {
+  logout() {
     this.logOut.logOut();
-  }
-
-  onDropdownItemChose(item: DropdownModel<string>) {
-    this.router.navigate([item.value]).then();
   }
 }
