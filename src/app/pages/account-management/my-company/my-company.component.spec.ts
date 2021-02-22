ada2634f6eb934de6d90b2c8aed370ae9cee7954
Alex Tkachenko <alex@softkit.company>
2021-02-22 16:06:21 +0200

    update angular version

--- src/app/pages/account-management/my-company/my-company.component.spec.ts
+++ src/app/pages/account-management/my-company/my-company.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {MyCompanyComponent} from './my-company.component';
 
@@ -6,7 +6,7 @@ describe('MyCompanyComponent', () => {
   let component: MyCompanyComponent;
   let fixture: ComponentFixture<MyCompanyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyCompanyComponent ]
     })
--- src/app/pages/applications/app-search/app-search.component.spec.ts
+++ src/app/pages/applications/app-search/app-search.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AppSearchComponent } from './app-search.component';
 
@@ -6,7 +6,7 @@ describe('AppSearchComponent', () => {
   let component: AppSearchComponent;
   let fixture: ComponentFixture<AppSearchComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AppSearchComponent ]
     })
