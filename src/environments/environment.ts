72a1c68c40e789b044fa83517a0391371bbf0193
Alex Tkachenko <pavellarionov@softkit.company>
2021-03-26 18:46:41 +0200

    hotfix management table 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://stage1-client-api-proxy.openchannel.io/',
+  apiUrl: 'http://localhost:8091/',
   marketplaceName: 'OpenChannel'
 };
 
