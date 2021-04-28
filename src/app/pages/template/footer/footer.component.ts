b4e2f626a9d5927a639850c31986fcd7fd13bd88
Pavel <pavellarionov@softkit.company>
2021-04-28 15:59:38 +0300

    social link component

--- src/app/pages/template/footer/footer.component.ts
+++ src/app/pages/template/footer/footer.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  socialLinks = [
+    {
+      link: 'https://facebook.com',
+      iconSrc: 'assets/img/facebook-icon.svg',
+      iconAlt: 'facebook-icon'
+    },
+    {
+      link: 'https://twitter.com',
+      iconSrc: 'assets/img/twitter-icon.svg',
+      iconAlt: 'twitter-icon'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
