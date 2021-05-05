e12184c134f8edd818601dd92cfbc7a866927ce8
Pavel <pavellarionov@softkit.company>
2021-05-05 16:43:25 +0300

    changed indents to 4

--- src/app/pages/template/footer/footer.component.ts
+++ src/app/pages/template/footer/footer.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
 
-  socialLinks = [
-    {
-      link: 'https://facebook.com',
-      iconSrc: 'assets/img/facebook-icon.svg',
-      iconAlt: 'facebook-icon'
-    },
-    {
-      link: 'https://twitter.com',
-      iconSrc: 'assets/img/twitter-icon.svg',
-      iconAlt: 'twitter-icon'
+    socialLinks = [
+        {
+            link: 'https://facebook.com',
+            iconSrc: 'assets/img/facebook-icon.svg',
+            iconAlt: 'facebook-icon'
+        },
+        {
+            link: 'https://twitter.com',
+            iconSrc: 'assets/img/twitter-icon.svg',
+            iconAlt: 'twitter-icon'
+        }
+    ];
+
+    constructor() {
     }
-  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 }
