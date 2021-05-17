757c7cf8b789fd24892a25a5867cbf7f694739ce
Alex Tkachenko <alex@softkit.company>
2021-05-17 12:58:15 +0300

    AT-1196 fixed display of social links fixed import path

--- src/app/pages/template/footer/footer.component.ts
+++ src/app/pages/template/footer/footer.component.ts
@@ -1,29 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {SocialLink} from 'oc-ng-common-component/src/lib/common-components/interfaces/social-link.model';
+import { Component } from '@angular/core';
+import { SocialLink } from '@openchannel/angular-common-components/src/lib/common-components/interfaces/social-link.model';
 
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss']
+    styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-
+export class FooterComponent {
     socialLinks: SocialLink[] = [
         {
             link: 'https://facebook.com',
             iconSrc: 'assets/img/facebook-icon.svg',
-            iconAlt: 'facebook-icon'
+            iconAlt: 'facebook-icon',
         },
         {
             link: 'https://twitter.com',
             iconSrc: 'assets/img/twitter-icon.svg',
-            iconAlt: 'twitter-icon'
-        }
+            iconAlt: 'twitter-icon',
+        },
     ];
 
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
+    constructor() {}
 }
