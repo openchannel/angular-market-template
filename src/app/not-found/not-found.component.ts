de8e07559d829d3b541281f621174f2eac739029
Julia <yui@softkit.company>
2021-08-09 14:04:35 +0300

    AT-1200: Added 404 error case

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { PrerenderRequestsWatcherService } from '@openchannel/angular-common-services';
 
 @Component({
-  selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['./not-found.component.scss'],
 })
 export class NotFoundComponent implements OnInit {
+    constructor(private router: Router, private prerenderService: PrerenderRequestsWatcherService) {}
 
-  constructor(private router: Router) { }
+    ngOnInit(): void {
+        this.prerenderService.create404MetaTag();
+    }
 
-  ngOnInit(): void {
-  }
-
-  goToHomePage() {
-    this.router.navigate(['/']).then(() => window.scrollTo(0, 0));
-  }
+    goToHomePage(): void {
+        this.prerenderService.remove404MetaTag();
+        this.router.navigate(['/']).then(() => window.scrollTo(0, 0));
+    }
 }
