7a9c233b521369ac84d0a4bb737878d81fcde7e0
Julia <yui@softkit.company>
2021-08-13 18:00:54 +0300

    AT-1200: removed 301 endpoints from the config

--- src/assets/data/prerenderEndpoints.ts
+++ src/assets/data/prerenderEndpoints.ts
@@ -2,5 +2,4 @@ import { PrerenderEndpointsConfig } from '@openchannel/angular-common-services';
 
 export const prerenderEndpoints: PrerenderEndpointsConfig = {
     excludeAPICall: ['/v2/stats/increment/'],
-    error301: ['/v2/apps/bySafeName/'],
 };
