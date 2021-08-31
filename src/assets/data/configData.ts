98e61808f615fc396d3bd17b6fd7fe3b6222a41f
Alex <alex@softkit.company>
2021-08-31 14:01:54 +0000

    Merged in hotfix/AT-1375_app_statistics (pull request #372)
    AT-1375 added app statistic sending
    
    * AT-1375 added app statistic sending
    
    * Hotfix pre-release 1.18.1-0
    
    * AT-1375 added statistic field to buttonAction
    
    * Up repository version to 1.27.1. Added changelog.md. Up service and components version to 2.18.1.
    
    
    Approved-by: LarionovPavel
    Approved-by: Julia
    Approved-by: Виталий Самофал

--- src/assets/data/configData.ts
+++ src/assets/data/configData.ts
@@ -1,92 +1,96 @@
- export const pageConfig = {
-   "fieldMappings": {
-     "icon": "icon",
-     "logo": "logo",
-     "summary": "summary",
-     "description": "description",
-     "video": "video-url",
-     "images": "images",
-     "categories": "categories",
-     "author": "author",
-     "gallery": "gallery"
-   },
-   "appListPage": [
-     {
-       "name": "Your App Store Platform",
-       "description": "A design template for implementing your app store with OpenChannel",
-       "type": "search"
-     },
-     {
-       "name": "Featured",
-       "description": "",
-       "type": "featured-apps",
-       "filter": "{\"attributes.featured\": \"yes\"}",
-       "filterId": "collections",
-       "valueId": "featured",
-       "sort": "{randomize: 1}"
-     }
-   ],
-   "appDetailsPage": {
-     "listing-actions": [
-       {
-         "type": "form",
-         "appTypes": ["web_plugin", "marketing", "code_with_fun"],
-         "formId": "buy-now",
-         "message": {
-           "success": "Thank you for submitting, we'll get back to you shortly",
-           "fail": "Due to an error, we were not able to send your form"
-         },
-         "button": {
-           "class": "oc-button_primary",
-           "text": "Buy now"
-         }
-       },
-       {
-         "type": "form",
-         "appTypes": ["web_plugin", "marketing", "code_with_fun"],
-         "formId": "contact-us",
-         "message": {
-           "success": "Thank you for submitting, we'll get back to you shortly",
-           "fail": "Due to an error, we were not able to send your form"
-         },
-         "button": {
-           "class": "oc-button_secondary",
-           "text": "Contact"
-         }
-       },
-       {
-         "type": "install",
-         "appTypes": ["web_plugin", "web_integration", "internal_feature"],
-         "unowned": {
-           "message": {
-             "success": "Your app has been installed",
-             "fail": "Due to an error we were not able to install your app"
-           },
-           "button": {
-             "class": "oc-button_primary",
-             "text": "Install",
-           }
-         },
-         "owned": {
-           "message": {
-             "success": "Your app has been uninstalled",
-             "fail": "Due to an error we were not able to uninstall your app"
-           },
-           "button": {
-             "class": "oc-button_primary",
-             "text": "Uninstall",
-           }
-         }
-       },
-       {
-         "type":"download",
-         "appTypes": ["downloadable"],
-         "fileField": "customData.releases[0].file",
-         "button": {
-           "class": "oc-button_primary",
-           "text": "Download"
-         }
-       }
-     ]
-   }
- }
+export const pageConfig = {
+    fieldMappings: {
+        icon: 'icon',
+        logo: 'logo',
+        summary: 'summary',
+        description: 'description',
+        video: 'video-url',
+        images: 'images',
+        categories: 'categories',
+        author: 'author',
+        gallery: 'gallery',
+    },
+    appListPage: [
+        {
+            name: 'Your App Store Platform',
+            description: 'A design template for implementing your app store with OpenChannel',
+            type: 'search',
+        },
+        {
+            name: 'Featured',
+            description: '',
+            type: 'featured-apps',
+            filter: '{"attributes.featured": "yes"}',
+            filterId: 'collections',
+            valueId: 'featured',
+            sort: '{randomize: 1}',
+        },
+    ],
+    appDetailsPage: {
+        'listing-actions': [
+            {
+                type: 'form',
+                appTypes: ['web_plugin', 'marketing', 'code_with_fun'],
+                formId: 'buy-now',
+                message: {
+                    success: "Thank you for submitting, we'll get back to you shortly",
+                    fail: 'Due to an error, we were not able to send your form',
+                },
+                button: {
+                    class: 'oc-button_primary',
+                    text: 'Buy now',
+                },
+                statistic: 'installs',
+            },
+            {
+                type: 'form',
+                appTypes: ['web_plugin', 'marketing', 'code_with_fun'],
+                formId: 'contact-us',
+                message: {
+                    success: "Thank you for submitting, we'll get back to you shortly",
+                    fail: 'Due to an error, we were not able to send your form',
+                },
+                button: {
+                    class: 'oc-button_secondary',
+                    text: 'Contact',
+                },
+                statistic: 'leads',
+            },
+            {
+                type: 'install',
+                appTypes: ['web_plugin', 'web_integration', 'internal_feature'],
+                unowned: {
+                    message: {
+                        success: 'Your app has been installed',
+                        fail: 'Due to an error we were not able to install your app',
+                    },
+                    button: {
+                        class: 'oc-button_primary',
+                        text: 'Install',
+                    },
+                },
+                owned: {
+                    message: {
+                        success: 'Your app has been uninstalled',
+                        fail: 'Due to an error we were not able to uninstall your app',
+                    },
+                    button: {
+                        class: 'oc-button_primary',
+                        text: 'Uninstall',
+                    },
+                },
+                statistic: 'installs',
+            },
+            {
+                type: 'download',
+                appTypes: ['downloadable'],
+                fileField: 'customData.releases[0].file',
+                button: {
+                    class: 'oc-button_primary',
+                    text: 'Download',
+                },
+                statistic: 'downloads',
+            },
+        ],
+    },
+};
