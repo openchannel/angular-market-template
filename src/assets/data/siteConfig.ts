d4b119451e81bc36f4deb7f3a69515dfa3acdfd5
Виталий Самофал <samofal.vitaliy@gmail.com>
2021-09-07 14:13:24 +0000

    Merged in feature/AT-1201_meta_tags (pull request #384)
    AT-1201 & AT-1203. Fixed meta tags configuration.
    
    * AT-1201 & AT-1203. Fixed meta tags configuration.
    
    
    Approved-by: Alex

--- src/assets/data/siteConfig.ts
+++ src/assets/data/siteConfig.ts
@@ -3,43 +3,17 @@ import { MetaTagsPageConfig, SiteConfig } from '@openchannel/angular-common-serv
 export const metaTags: MetaTagsPageConfig = {
     defaultMetaTags: [
         { name: 'author', content: 'OpenChannel' },
-        { name: 'description', content: 'OpenChannel' },
         { name: 'generator', content: 'OpenChannel' },
         { name: 'og:url', definitionPath: 'windowUrl' },
-        { name: 'og:title', content: 'OpenChannel' },
-        { name: 'og:image', content: 'OpenChannel' },
-        { name: 'og:description', content: 'OpenChannel' },
     ],
     pages: [
         {
-            routerLinkStartsWith: '/',
-            metaTags: [
-                { name: 'description', content: 'OpenChannel' },
-                { name: 'og:title', content: 'OpenChannel' },
-                { name: 'og:image', content: 'OpenChannel' },
-                { name: 'og:description', content: 'OpenChannel' },
-            ],
-        },
-        {
-            routerLinkStartsWith: '/browse',
-            metaTags: [
-                { name: 'description', content: 'OpenChannel' },
-                { name: 'og:title', content: 'OpenChannel' },
-                { name: 'og:image', content: 'OpenChannel' },
-                { name: 'og:description', content: 'OpenChannel' },
-            ],
-        },
-        {
             routerLinkStartsWith: '/details',
             metaTags: [
-                {
-                    name: 'description',
-                    content: 'OpenChannel',
-                    definitionPath: 'app.customData.summary',
-                },
-                { name: 'og:title', content: 'OpenChannel', definitionPath: 'app.name' },
-                { name: 'og:image', content: 'OpenChannel', definitionPath: 'app.customData.logo' },
-                { name: 'og:description', content: 'OpenChannel', definitionPath: 'app.customData.description' },
+                { name: 'description', content: '', definitionPath: 'app.customData.summary' },
+                { name: 'og:title', content: '', definitionPath: 'app.name' },
+                { name: 'og:image', content: '', definitionPath: 'app.customData.logo' },
+                { name: 'og:description', content: '', definitionPath: 'app.customData.description' },
             ],
         },
     ],
