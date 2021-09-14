dd62eff04bf66eaf61403cc0ee94e6dac834a8a8
Julia <yui@softkit.company>
2021-09-14 19:06:25 +0300

    Minor update with develop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8091/',
+    apiUrl: 'https://client-api.openchannel.io/',
     marketplaceName: 'OpenChannel',
     enableProxy: false,
     enableCMS: true,
