f224a42a5b2bd8df59031e15fe6f28df6b84687f
Julia <yui@softkitit.com>
2021-11-03 17:09:55 +0200

    AT-1586: updated the Shared section

--- src/app/shared/components/page-title/page-title.component.spec.ts
+++ src/app/shared/components/page-title/page-title.component.spec.ts
@@ -1,25 +1,24 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {PageTitleComponent} from './page-title.component';
+import { PageTitleComponent } from './page-title.component';
 
 describe('PageTitleComponent', () => {
-  let component: PageTitleComponent;
-  let fixture: ComponentFixture<PageTitleComponent>;
+    let component: PageTitleComponent;
+    let fixture: ComponentFixture<PageTitleComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PageTitleComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PageTitleComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PageTitleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PageTitleComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/shared/pipes/file-size.pipe.ts
+++ src/app/shared/pipes/file-size.pipe.ts
@@ -31,29 +31,21 @@ import { Pipe, PipeTransform } from '@angular/core';
  * Example:
  *   {{ 1024 |  fileSize}}
  *   formats to: 1 KB
-*/
-@Pipe({name: 'fileSize'})
+ */
+@Pipe({ name: 'fileSize' })
 export class FileSizePipe implements PipeTransform {
+    private units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB'];
 
-  private units = [
-    'bytes',
-    'KB',
-    'MB',
-    'GB',
-    'TB',
-    'PB'
-  ];
+    transform(bytes: number = 0, precision: number = 2): string {
+        if (isNaN(parseFloat(String(bytes))) || !isFinite(bytes)) return '?';
 
-  transform(bytes: number = 0, precision: number = 2 ) : string {
-    if ( isNaN( parseFloat( String(bytes) )) || ! isFinite( bytes ) ) return '?';
+        let unit = 0;
 
-    let unit = 0;
+        while (bytes >= 1024) {
+            bytes /= 1024;
+            unit++;
+        }
 
-    while ( bytes >= 1024 ) {
-      bytes /= 1024;
-      unit ++;
+        return bytes.toFixed(+precision) + ' ' + this.units[unit];
     }
-
-    return bytes.toFixed( + precision ) + ' ' + this.units[ unit ];
-  }
 }
