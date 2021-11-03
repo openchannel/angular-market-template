549a3fa70cd269bd40a1e18366987aa238e6a40a
Julia <yui@softkitit.com>
2021-11-03 16:36:42 +0200

    AT-1586: updated the My company

--- src/app/pages/account-management/my-company/my-company.component.spec.ts
+++ src/app/pages/account-management/my-company/my-company.component.spec.ts
@@ -1,25 +1,26 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import {MyCompanyComponent} from './my-company.component';
+import { MyCompanyComponent } from './my-company.component';
 
 describe('MyCompanyComponent', () => {
-  let component: MyCompanyComponent;
-  let fixture: ComponentFixture<MyCompanyComponent>;
+    let component: MyCompanyComponent;
+    let fixture: ComponentFixture<MyCompanyComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MyCompanyComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [MyCompanyComponent],
+            }).compileComponents();
+        }),
+    );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyCompanyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MyCompanyComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
