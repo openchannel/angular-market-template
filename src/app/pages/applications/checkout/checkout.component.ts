915d325ad6b23e42d8d016f786ce85179e0016f2
Julia <yui@softkitit.com>
2021-11-25 13:06:57 +0200

    AT-1563: added a billing form to the Checkout page

--- src/app/pages/applications/checkout/checkout.component.ts
+++ src/app/pages/applications/checkout/checkout.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { StripeLoaderService } from '@core/services/stripe-loader.service';
 
 @Component({
-  selector: 'app-checkout',
-  templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+    selector: 'app-checkout',
+    templateUrl: './checkout.component.html',
+    styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
+    constructor(private stripeLoader: StripeLoaderService) {}
 
-  constructor(private stripeLoader: StripeLoaderService) { }
-
-  ngOnInit(): void {
-    this.stripeLoader.loadStripe();
-  }
-
+    ngOnInit(): void {
+        this.stripeLoader.loadStripe();
+    }
+    goBack(): void {
+        history.back();
+    }
 }
