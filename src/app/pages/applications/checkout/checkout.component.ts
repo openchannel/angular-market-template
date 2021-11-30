dae34f35db8d0637c16bff9fc9df9412e080e5e4
Julia <yui@softkitit.com>
2021-11-30 11:58:45 +0200

    AT-1563: added app request

--- src/app/pages/applications/checkout/checkout.component.ts
+++ src/app/pages/applications/checkout/checkout.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { StripeLoaderService } from '@core/services/stripe-loader.service';
+import { AppsService, AppVersionService } from '@openchannel/angular-common-services';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingBarState } from '@ngx-loading-bar/core/loading-bar.state';
+import { LoadingBarService } from '@ngx-loading-bar/core';
+import { Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+import { FullAppData } from '@openchannel/angular-common-components';
+import { pageConfig } from 'assets/data/configData';
 
 @Component({
     selector: 'app-checkout',
@@ -7,12 +15,61 @@ import { StripeLoaderService } from '@core/services/stripe-loader.service';
     styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-    constructor(private stripeLoader: StripeLoaderService) {}
+    app: FullAppData;
+
+    private loader: LoadingBarState;
+    private $destroy: Subject<void> = new Subject<void>();
+
+    constructor(
+        private stripeLoader: StripeLoaderService,
+        private appService: AppsService,
+        private activeRoute: ActivatedRoute,
+        private loadingBar: LoadingBarService,
+        private appVersionService: AppVersionService,
+    ) {}
 
     ngOnInit(): void {
+        this.loader = this.loadingBar.useRef();
+
         this.stripeLoader.loadStripe();
+        this.loadAppData();
     }
     goBack(): void {
         history.back();
+    }
+
+    private loadAppData(): void {
+        this.loader.start();
+
+        const appId = this.activeRoute.snapshot.paramMap.get('appId');
+        const appVersion = this.activeRoute.snapshot.paramMap.get('appVersion');
+        const safeName = this.activeRoute.snapshot.paramMap.get('safeName');
+
+        const appRequest = safeName
+            ? this.appService.getAppBySafeName(safeName)
+            : this.appVersionService.getAppByVersion(appId, Number(appVersion));
+
+        appRequest
+            .pipe(
+                takeUntil(this.$destroy),
+                map(app => {
+                    const mappedApp = new FullAppData(app, pageConfig.fieldMappings);
+                    if (typeof mappedApp.images[0] === 'string') {
+                        mappedApp.images = (mappedApp.images as string[]).map(imageItem => {
+                            return {
+                                image: imageItem,
+                            };
+                        });
+                    }
+                    return mappedApp;
+                }),
+            )
+            .subscribe(
+                app => {
+                    this.app = app;
+                    this.loader.complete();
+                },
+                () => this.loader.complete(),
+            );
     }
 }
