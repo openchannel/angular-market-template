eca27039af1673477513bd8cc0311e6ce25be33d
Pavel <pavellarionov@softkit.company>
2021-12-27 17:47:04 +0200

    bugfix AT-1612 fixed wrong page number in apps request

--- src/app/pages/account-management/my-apps/my-apps.component.ts
+++ src/app/pages/account-management/my-apps/my-apps.component.ts
@@ -67,8 +67,10 @@ export class MyAppsComponent implements OnInit, OnDestroy {
     }
 
     onScrollDown(): void {
-        this.pageNumber++;
-        this.loadApps();
+        if (this.appList.length > 0) {
+            this.pageNumber++;
+            this.loadApps();
+        }
     }
 
     goBack(): void {
