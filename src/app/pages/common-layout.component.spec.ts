847de90db512f9a8f5e60b95e0a04b5d2b00d2e7
Andrey Glazkov <aglazkov@softkitit.com>
2022-03-30 18:34:16 +0300

    AT-1752: skip common layout tests

--- src/app/pages/common-layout.component.spec.ts
+++ src/app/pages/common-layout.component.spec.ts
@@ -2,24 +2,25 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CommonLayoutComponent } from './common-layout.component';
 
-describe('CommonLayoutComponent', () => {
-  let component: CommonLayoutComponent;
-  let fixture: ComponentFixture<CommonLayoutComponent>;
+describe.skip('CommonLayoutComponent', () => {
+    let component: CommonLayoutComponent;
+    let fixture: ComponentFixture<CommonLayoutComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CommonLayoutComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                declarations: [CommonLayoutComponent],
+            }).compileComponents();
+        }),
+    );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CommonLayoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CommonLayoutComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
