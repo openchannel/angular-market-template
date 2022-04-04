1e61187a4a426cfd7a6b5d852be4c2b7d73ce238
Pavel <pavellarionov@softkit.company>
2022-04-04 14:54:41 +0300

    feature/AT-1654-unit-tests-change-password

--- src/app/pages/account-management/my-profile/change-password/change-password.component.spec.ts
+++ src/app/pages/account-management/my-profile/change-password/change-password.component.spec.ts
@@ -1,6 +1,10 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync, fakeAsync, tick } from '@angular/core/testing';
 
 import { ChangePasswordComponent } from './change-password.component';
+import { NativeLoginService } from '@openchannel/angular-common-services';
+import { MockNativeLoginService, MockToastrService } from '../../../../../mock/mock';
+import { throwError } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 describe.skip('ChangePasswordComponent', () => {
     let component: ChangePasswordComponent;
@@ -10,9 +14,17 @@ describe.skip('ChangePasswordComponent', () => {
         waitForAsync(() => {
             TestBed.configureTestingModule({
                 declarations: [ChangePasswordComponent],
+                providers: [
+                    { provide: NativeLoginService, useClass: MockNativeLoginService },
+                    { provide: ToastrService, useClass: MockToastrService },
+                ],
             }).compileComponents();
         }),
     );
+    
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(ChangePasswordComponent);
@@ -21,6 +33,22 @@ describe.skip('ChangePasswordComponent', () => {
     });
 
     it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+    
+    it('should set password formGroup', () => {
+        expect(component).toBeTruthy();
+    });
+    
+    it('return if isSaveInProcess or passwordFormGroup is invalid', () => {
+        expect(component).toBeTruthy();
+    });
+    
+    it('return if isSaveInProcess or passwordFormGroup is invalid', () => {
+        expect(component).toBeTruthy();
+    });
+    
+    it('change password and show toaster message', () => {
         expect(component).toBeTruthy();
     });
 });
