6b9ff9616a03296e3d03619f1db02c0363dc9632
Artem Koteluk <akoteliuk@softkitit.com>
2022-04-05 17:30:00 +0300

    AT-1663-tests-for-app-resend-activation-component: reworked tests due to comments

--- src/app/pages/general/resend-activation/resend-activation.component.spec.ts
+++ src/app/pages/general/resend-activation/resend-activation.component.spec.ts
@@ -1,16 +1,17 @@
-import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { ResendActivationComponent } from './resend-activation.component';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NativeLoginService } from '@openchannel/angular-common-services';
 import { MockNativeLoginService, MockResendActivation, MockRoutingComponent, MockToastrService } from '../../../../mock/mock';
-import { throwError } from 'rxjs';
+import { asyncScheduler, throwError } from 'rxjs';
 import { By } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
 import { Location } from '@angular/common';
+import { observeOn } from 'rxjs/operators';
 
-describe.skip('ResendActivationComponent', () => {
+describe('ResendActivationComponent', () => {
     let component: ResendActivationComponent;
     let fixture: ComponentFixture<ResendActivationComponent>;
     let router: Router;
@@ -18,36 +19,30 @@ describe.skip('ResendActivationComponent', () => {
 
     const getActivationResendDE = () => fixture.debugElement.query(By.directive(MockResendActivation));
 
-    beforeEach(
-        waitForAsync(() => {
-            TestBed.configureTestingModule({
-                declarations: [ResendActivationComponent, MockResendActivation],
-                imports: [RouterTestingModule.withRoutes([{ path: 'login', component: MockRoutingComponent }])],
-                providers: [
-                    { provide: NativeLoginService, useClass: MockNativeLoginService },
-                    { provide: ToastrService, useClass: MockToastrService },
-                ],
-            }).compileComponents();
-        }),
-    );
-
     beforeEach(() => {
-        jest.resetAllMocks();
-    });
+        TestBed.configureTestingModule({
+            declarations: [ResendActivationComponent, MockResendActivation],
+            imports: [
+                RouterTestingModule.withRoutes([
+                    { path: 'login', component: MockRoutingComponent },
+                    { path: 'resend-activation', component: ResendActivationComponent },
+                ]),
+            ],
+            providers: [
+                { provide: NativeLoginService, useClass: MockNativeLoginService },
+                { provide: ToastrService, useClass: MockToastrService },
+            ],
+        }).compileComponents();
 
-    // Setup router for the component
-    beforeEach(() => {
-        const routerState = { extras: { state: {} } };
-
-        location = TestBed.inject(Location);
-        router = TestBed.inject(Router);
-        router.getCurrentNavigation = jest.fn().mockReturnValue(routerState);
-    });
-
-    beforeEach(() => {
         fixture = TestBed.createComponent(ResendActivationComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+
+        jest.resetAllMocks();
+
+        // Setup router for the component
+        location = TestBed.inject(Location);
+        router = TestBed.inject(Router);
     });
 
     it('should create', () => {
@@ -55,40 +50,49 @@ describe.skip('ResendActivationComponent', () => {
     });
 
     it('should call nativeLoginService.sendActivationCode and to check response success actions', fakeAsync(() => {
-        jest.spyOn((component as any).nativeLoginService, 'sendActivationCode');
+        jest.spyOn((component as any).toaster, 'success');
+
+        component.inProcess = false;
 
         const ocResendActivationMock = getActivationResendDE().componentInstance;
         ocResendActivationMock.buttonClick.emit(true);
-        tick();
 
         expect(component.inProcess).toBeTruthy();
+
+        tick();
+
+        expect(component.inProcess).toBeFalsy();
         expect((component as any).toaster.success).toHaveBeenCalled();
         expect(location.path()).toBe('/login');
     }));
 
     it('should set inProcess=false when nativeLoginService.sendActivationCode throws error', fakeAsync(() => {
+        const path = location.path();
         const ocResendActivationMock = getActivationResendDE().componentInstance;
+        (component as any).nativeLoginService.sendActivationCode = () => throwError('Error').pipe(observeOn(asyncScheduler));
         ocResendActivationMock.buttonClick.emit(true);
+
+        expect(component.inProcess).toBeTruthy();
+
         tick();
 
-        (component as any).nativeLoginService.sendActivationCode = () => throwError('Error');
-
         expect(component.inProcess).toBeFalsy();
+        expect(location.path()).toBe(path);
     }));
 
     it('should pass all necessary variables to the app-resend-activation', () => {
-        const customActivationResendBindingsComponentPropsMap = {
-            activationModel: 'activationModel',
-            companyLogoUrl: 'companyLogoUrl',
-            loginUrl: 'loginUrl',
-            signupUrl: 'signupUrl',
-            process: 'inProcess',
+        component.activationModel = {
+            password: '123123123',
+            email: 'mail@mail.com',
+            code: '123123123',
         };
+        component.inProcess = true;
+
+        fixture.detectChanges();
 
         const activationResendInstance = getActivationResendDE().componentInstance;
 
-        Object.values(customActivationResendBindingsComponentPropsMap).forEach(([binding, propName]) => {
-            expect(activationResendInstance[binding]).toEqual(component[propName]);
-        });
+        expect(activationResendInstance.activationModel).toEqual(component.activationModel);
+        expect(activationResendInstance.process).toBeTruthy();
     });
 });
