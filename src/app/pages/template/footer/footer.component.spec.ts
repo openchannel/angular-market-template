2d979544ca78047414e08efe9abbf6f45530666a
Artem Koteluk <akoteliuk@softkitit.com>
2022-04-06 13:03:36 +0300

    AT-1663-tests-for-app-resend-activation-component: reworked tests due to comments

--- src/app/pages/template/footer/footer.component.spec.ts
+++ src/app/pages/template/footer/footer.component.spec.ts
@@ -1,57 +1,36 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
 import { MockCmsContentService, MockSocialLinks } from '../../../../mock/mock';
 import { CmsContentService } from '@core/services/cms-content-service/cms-content-service.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
-import { get } from 'lodash';
 
-describe.skip('FooterComponent', () => {
+describe('FooterComponent', () => {
     let component: FooterComponent;
     let fixture: ComponentFixture<FooterComponent>;
 
     const getSocialLinksDE = () => fixture.debugElement.query(By.directive(MockSocialLinks));
 
-    beforeEach(
-        waitForAsync(() => {
-            TestBed.configureTestingModule({
-                declarations: [FooterComponent, MockSocialLinks],
-                imports: [RouterTestingModule],
-                providers: [{ provide: CmsContentService, useClass: MockCmsContentService }],
-            }).compileComponents();
-        }),
-    );
-
     beforeEach(() => {
-        jest.resetAllMocks();
-    });
+        TestBed.configureTestingModule({
+            declarations: [FooterComponent, MockSocialLinks],
+            imports: [RouterTestingModule],
+            providers: [{ provide: CmsContentService, useClass: MockCmsContentService }],
+        }).compileComponents();
 
-    beforeEach(() => {
         fixture = TestBed.createComponent(FooterComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+
+        jest.resetAllMocks();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should call initCMSData method in ngOnInit hook', () => {
-        jest.spyOn(component as any, 'initCMSData');
-
-        // tslint:disable-next-line:no-lifecycle-call
-        component.ngOnInit();
-
-        expect((component as any).initCMSData).toHaveBeenCalled();
-    });
-
-    it('should set correct cmsData in initCMSData', () => {
-        const namePathMap = {
-            logoImageURL: 'default-footer.logo',
-            columnsDFA: 'default-footer.menu.items',
-        };
-
+    it('should set correct cmsData in initCMSData', fakeAsync(() => {
         component.cmsData = {
             logoImageURL: '',
             columnsDFA: [],
@@ -59,20 +38,29 @@ describe.skip('FooterComponent', () => {
 
         component.initCMSData();
 
-        Object.entries(namePathMap).forEach(([name, path]) => {
-            expect(component.cmsData[name]).toBe(get(MockCmsContentService.CMS_DATA, path));
-        });
-    });
+        tick();
+
+        expect(component.cmsData.logoImageURL).toBe(MockCmsContentService.CMS_DATA['default-footer'].logo);
+        expect(component.cmsData.columnsDFA).toStrictEqual(MockCmsContentService.CMS_DATA['default-footer'].menu.items);
+    }));
 
     it('should pass all necessary variables to the oc-social-links', () => {
-        const socialLinksBindingsComponentPropsMap = {
-            socialLinks: 'socialLinks',
-        };
+        component.socialLinks = [
+            {
+                link: 'https://facebook.com',
+                iconSrc: 'assets/img/facebook-icon.svg',
+                iconAlt: 'facebook-icon',
+            },
+            {
+                link: 'https://twitter.com',
+                iconSrc: 'assets/img/twitter-icon.svg',
+                iconAlt: 'twitter-icon',
+            },
+        ];
+
+        fixture.detectChanges();
 
         const socialLinksInstance = getSocialLinksDE().componentInstance;
-
-        Object.values(socialLinksBindingsComponentPropsMap).forEach(([binding, propName]) => {
-            expect(socialLinksInstance[binding]).toEqual(component[propName]);
-        });
+        expect(socialLinksInstance.socialLinks).toEqual(component.socialLinks);
     });
 });
